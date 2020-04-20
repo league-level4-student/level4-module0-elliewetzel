f62de5db6c776a0a6e1186bd0f6611df1e1f62cf
Ellie Wetzel <elliehw2005@gmail.com>
2020-04-19 17:29:25 -0700

    CrazyDigitalPainting, GridPanel(IP)

--- src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
+++ src/_01_Crazy_Digital_Painting/CrazyDigitalPainting.java
@@ -1,13 +1,15 @@
 package _01_Crazy_Digital_Painting;
 
+import java.awt.Color;
+
 public class CrazyDigitalPainting {
 	//1. Create two final static integers for the width and height of the display.
-
-	
+	final static int width = 100;
+	final static int height = 300;
 	//2. Create a 2D array of Color objects. You will need to import
 	//java.awt.Color. Initialize the size of the array using the 
 	//integers created in step 1.
-	
+	Color[][] c = new Color[width][height];
 	
 	
 	public CrazyDigitalPainting() {
@@ -17,11 +19,17 @@ public class CrazyDigitalPainting {
 		//   to a new color. The sample image was created using the following 
 		//   pattern:
 		//   colors[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
-		
+		for(int i = 0; i < c.length; i++) {
+			for(int j = 0; j < c[0].length; j++) {
+			c[i][j] = new Color(i % 256, (i * j) % 256, j % 256);
+			}
+		}
 		//5. Come up with your own pattern to make a cool crazy image.
 		
 		//6. Use the ColorArrayDisplayer class to call the displayColorsAsImage method 
 		//   to show off your picture.
+		//ColorArrayDisplayer dis = new ColorArrayDisplayer();
+		ColorArrayDisplayer.displayColorsAsImage(c);
 	}
 	
 	public static void main(String[] args) {
